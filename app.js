c3bc89b176443228880df0af4f5985dfe834686e
amh5366 <amh5366@upenn.edu>
2018-08-01 16:00:27 -0400

    Added inquirer and a prompt

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
 
@@ -17,10 +18,26 @@ let listProducts = function () {
             console.error(err);
         }
 
-        console.log(res);
         for(let i = 0; i < res.length; i++) {
             console.log(`ID: ${res[i].item_id}\tITEM: ${res[i].product_name}\tPRICE: $${res[i].price}\tSTOCK: ${res[i].stock_quantity}`);
         }
+
+        inquirer.prompt([
+            {
+                "type": "input",
+                "name": "id",
+                "message": "What is the ID of the item you wish to buy?"
+            },
+            {
+                "type": "input",
+                "name": "quantity",
+                "message": "How man would you like to buy?"
+            }
+        ]).then(function (answer) {
+            console.log(answer.id);
+            console.log(answer.quantity);
+
+    });
     });
 };
 
